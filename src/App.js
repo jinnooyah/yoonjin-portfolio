fd41d747045229e88ef110d449dc2b962ea41e86
Yoonjin <yoonstruly@gmail.com>
2024-02-02 09:22:29 -0500

    changed route path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route
-          path="/about-me"
+          path="/"
           element={<AboutMe />} // <-- passed as JSX
         />
         <Route
