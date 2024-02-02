ff0fd615402f3b39a06fa8f71046398e70f30740
Yoonjin <yoonstruly@gmail.com>
2024-02-02 06:18:50 -0500

    changed comms

---
+++ src/pages/Comms.js
@@ -0,0 +1,14 @@
+import "./Comms.css";
+import "./global.css";
+import Header from "../components/Header";
+
+export default function Comms() {
+    return (
+        <div>
+            <Header />
+            <div className="page">
+                <p>hi</p>
+            </div>
+        </div>
+    )
+}
