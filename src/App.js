72a8b0b25ba53c628cbfc5c8a1673127c19dd5da
Yoonjin <yoonstruly@gmail.com>
2024-02-01 19:28:54 -0500

    helpppp idk

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route
-          path="/home"
+          path=""
           element={<Home />} // <-- passed as JSX
         />
         <Route
