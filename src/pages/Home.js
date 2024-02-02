073fb8f4ae9d3808bd0e2bea66d3e56024e90b68
Yoonjin <yoonstruly@gmail.com>
2024-02-01 19:20:40 -0500

    testing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,35 @@ export default function Home() {
         <body>
             <Header />
             <h1>jinojaem</h1>
+        <body>
+        <div class="container">
+            <div class="box window">
+                <div>
+                  <div className="image-container">
+                    <img src={cute} style={{width: "12rem"}} alt="cat" />
+
+                    <img src={youtube} style={{padding: "1rem", width: "2rem"}} onClick={onYoutubeClick}/>
+                  </div>
+                </div>
+
+                <div>
+                    <h2>about me!</h2>
+                    <table>
+                      <tr>
+                        <th style = {{width: "30%"}}>NAME</th>
+                        <th style = {{width: "40%"}}>DATE OF BIRTH</th>
+                        <th style = {{width: "30%"}}>PRONOUNS</th>
+                      </tr>
+                      <tr>
+                        <td>yoonjin</td>
+                        <td>sept 22, 2005</td>
+                        <td>she/her</td>
+                      </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
         </body>
     )
 }
