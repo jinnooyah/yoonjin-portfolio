0c796a6e0f4382ba427c685f0ed4ca3ded1296a4
Yoonjin <yoonstruly@gmail.com>
2025-08-24 05:47:20 -0400

    added HashRouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
+
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
